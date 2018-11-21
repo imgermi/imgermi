09fd3e7eb358eb1e63d60bc438bda82e9400684f
imgermi <juanmartingermano@gmail.com>
2018-11-21 13:05:36 -0300

    Borro codigos

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,21 +8,17 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Germi Portfolio' },
-      { name: 'google-site-verification', content: 'nIZHKw4qeDLe6fDDzkdCgyZ5pclkp-U45xqq-6LumMA' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700' }
-    ],
-    script: [
-      { src: '/js/fullstory.js' }
     ],
   },
   css: [
     '~/sass/main.sass'
   ],
   plugins: [
-    { src: '~plugins/ga.js', ssr: false }
+    
   ],
   /*
   ** Customize the progress bar color
